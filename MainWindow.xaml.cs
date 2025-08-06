842caae0010c575f0cb24e59e55a70757b878397
Shayan-Shams-2005 <shayanshams621@gmail.com>
2025-08-06 10:55:58 +0330

    Added my Project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System.DirectoryServices.ActiveDirectory;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+
+   
+
+
+   
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+        
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+          
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        { 
+
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void brnClear_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn1onX_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnX2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRX_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnCross_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnCE_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void btnNegation_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPoint_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEqual_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
